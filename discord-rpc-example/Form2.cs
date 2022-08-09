78a7ebe1b4ebb5e449461347ee084675452125b7
unknown <4afa3lbr@gmail.com>
2022-08-09 14:19:06 -0300

    first commit

---
+++ discord-rpc-example/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace discord_rpc_example
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Program.client.UpdateDetails("In Form2");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 GetForm1 = new Form1();
+            GetForm1.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ discord-rpc-example/Program.cs
@@ -0,0 +1,48 @@
+﻿using DiscordRPC;
+using System;
+using System.Windows.Forms;
+
+namespace discord_rpc_example
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Initialize();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        #region Discord RPC
+        public static DiscordRpcClient client;
+        static void Initialize()
+        {
+            // Create Application in https://discord.com/developers/applications and copy Application ID
+            client = new DiscordRpcClient("936284924179251220");
+            client.Initialize();
+
+            client.SetPresence(new RichPresence()
+            {
+                Details = "Starting...",
+                Assets = new Assets()
+                {
+                    LargeImageKey = "asdas",
+                    LargeImageText = "Example Discord RPC",
+                    SmallImageKey = "asdas",
+                    SmallImageText = "N/A",
+                },
+                Buttons = new DiscordRPC.Button[]
+                {
+                    new DiscordRPC.Button() { Label = "Join the Discord Server", Url = "https://www.google.com/" }
+                },
+                Timestamps = Timestamps.Now
+            });
+        }
+        #endregion
+    }
+}
